4ece12e60171a0c95664494526e6c42bf493c5f3
Feni <fenijuniarti@gmail.com>
2023-01-03 01:29:12 +0700

    home after

---
+++ TUBES/home.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ANS - Home Website</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+        <a href="home.php" class="navbar-brand ms-lg-5">
+            <img src="img/logo-ans.jpeg" width="150" height="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="home.php" class="nav-item nav-link active">Home</a>
+                <a href="about.php" class="nav-item nav-link">About Us</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Product/Service</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="price.html" class="dropdown-item">Product</a>
+                        <a href="blog.html" class="dropdown-item">Service</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Nama</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="profile.html" class="dropdown-item">Profile</a>
+                        <a href="logout.html" class="dropdown-item">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5">
+        <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/home.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h1 class="display-5 text-white mb-md-4">Leading Solution & IT Services</h1>
+                            <h7 class="text-white text-uppercase">Best Services are our foundation for succesfull long-term relationship with our customer</h7>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+    <!-- Carousel End -->
+    
+    <!-- Services Start -->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="text-center mx-auto mb-5" style="max-width: 600px;">
+                <h1 class="display-5 mb-0">Our Solution</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-cctv d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">Network</h5>
+                            <h3 class="mb-3">Solution</h3>
+                            <p>We are the solution for your Network need, Get some integration and security together for your system and data</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-surveillance d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">Network</h5>
+                            <h3 class="mb-3">Solution</h3>
+                            <p>We are the solution for your Network need, Get some integration and security together for your sytem and data</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-camera d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">System</h5>
+                            <h3 class="mb-3">Integration</h3>
+                            <p>We are the solution for your Network need, Get some integration and security together for your sytem and data</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-double d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">Hardware</h5>
+                            <h3 class="mb-3">Solution</h3>
+                            <p>We are the solution for your Network need, Get some integration and security together for your system and data</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-security-system d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">System</h5>
+                            <h3 class="mb-3">Solution</h3>
+                            <p>We are the solution for your Network neeed, Get some integration and security together for your sysytem and data</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item bg-light border-bottom border-5 border-primary rounded">
+                        <div class="position-relative p-5">
+                            <i class="flaticon-recorder d-block display-1 fw-normal text-secondary mb-3"></i>
+                            <h5 class="text-primary mb-0">Cable</h5>
+                            <h3 class="mb-3">Solution</h3>
+                            <p>We are the solution for your Network need, Get some integration and security together for your sytem and data</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Services End -->
+
+    <div class="container">
+        <div class="text-center mx-auto mb-5" style="max-width: 600px;">
+            <br>
+            <h5 class="display-5 mb-0"><strong>Our Partner</h5></strong>
+            </br>
+        </div>
+
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col"><img src="img/Yokke.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/Total.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/Jantung.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/Master-System.jpeg" width="150" height="100"></div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row align-items-center"></br></br><center></center>
+                <div class="col"><img src="img/Harkit.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/Mandiri.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/BPJS.jpeg" width="150" height="100"></div>
+                <div class="col"><img src="img/HDI.jpeg" width="150" height="100"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light mt-5 py-5">
+        <div class="container pt-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h3 class="text-white mb-4">Contact Us</h3>
+                    <p class="mb-2"><i class="bi bi-geo-alt text-primary me-2"></i>Jl. Maulana Hasanudin No.38A, Kp. Cipondoh Rt. 005 Rw. 008 Cipondoh Kota Tangerang 15148 Email: info@adita-ns.com Phone: 021 55712013</p>
+                    <p class="mb-2"><i class="bi bi-envelope-open text-primary me-2"></i>info@adita-ns.com</p>
+                    <p class="mb-0"><i class="bi bi-telephone text-primary me-2"></i>021 55712013</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3 class="text-white mb-4">Follow Us</h3>
+                    <div class="d-flex">
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-facebook-f fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded" href="#"><i class="fab fa-instagram fw-normal"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
