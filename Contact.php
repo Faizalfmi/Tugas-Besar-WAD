04235536732c6a271b0c7a0860a89b99489f7b7b
shilvychandrakirana <115609547+shilvychandrakirana@users.noreply.github.com>
2023-01-08 11:00:46 +0700

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ANS.COM</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+        <a href="Home_after.php" class="navbar-brand ms-lg-5">
+        <a class="navbar-brand" href="#"><img src="img/logo.jpeg" width="150" height="50"></a>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="Home_before.php" class="nav-item nav-link">Home</a>
+                <a href="About.php" class="nav-item nav-link">About Us</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Product/Service</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="price.html" class="dropdown-item">Service</a>
+                        <a href="blog.html" class="dropdown-item">Product</a>
+                    </div>
+                </div>
+                <a href="Contact.php" class="nav-item nav-link active">Contact Us</a>
+                <a href="Contact.php" class="nav-item nav-link">Login</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+   <!-- Carousel Start -->
+   <div class="container-fluid p-0 mb-5">
+        <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/home.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h1 class="display-5 text-white mb-md-4">Contact</h1>
+                            <h7 class="text-white text-uppercase">Best Services are our foundation for succesfull long-term relationship with our customer</h7>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+    <!-- Contact Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5" style="max-width: 600px;">
+                <h5 class="text-primary text-uppercase" style="letter-spacing: 5px;">Contact Us</h5>
+                <h1 class="display-5 mb-0">Get in touch with us</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <div class="bg-light rounded p-5">
+                        <form>
+                            <div class="row g-3">
+                                <div class="col-6">
+                                    <input type="text" class="form-control border-0 px-4" placeholder="First Name*" style="height: 55px;">
+                                </div>
+                                <div class="col-6">
+                                    <input type="email" class="form-control border-0 px-4" placeholder="Last Name*" style="height: 55px;">
+                                </div>
+                                <div class="col-6">
+                                    <input type="text" class="form-control border-0 px-4" placeholder="Your Email" style="height: 55px;">
+                                </div>
+                                <div class="col-6">
+                                    <input type="email" class="form-control border-0 px-4" placeholder="Website" style="height: 55px;">
+                                </div>
+                                
+                                <div class="col-12">
+                                    <textarea class="form-control border-0 px-4 py-3" rows="8" placeholder="Message"></textarea>
+                                </div>
+                                <div class="col-4">
+                                    <button class="btn btn-primary w-100 py-3" type="submit">Send</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="bg-light rounded p-5">
+                        <div class="d-flex align-items-center mb-2">
+                            <i class="bi bi-geo-alt fs-1 text-primary me-3"></i>
+                            <div class="text-start">
+                                <h5 class="mb-1">Head Office</h5>
+                                <span>Jl. Maulana Hasanudin No.38A, Kp. Cipondoh Rt. 005 Rw. 008, Cipondoh - Kota Tangerang 15148</span>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center mb-2">
+                            <i class="bi bi-envelope-open fs-1 text-primary me-3"></i>
+                            <div class="text-start">
+                                <h5 class="mb-1">Email Us</h5>
+                                <span>info@adita-ns.com</span>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center mb-4">
+                            <i class="bi bi-phone-vibrate fs-1 text-primary me-3"></i>
+                            <div class="text-start">
+                                <h5 class="mb-1">Call Us</h5>
+                                <span>021 55712013</span>
+                            </div>
+                        </div>
+                        <div>
+                            <iframe class="position-relative w-100"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15866.20013575064!2d106.6757387!3d-6.190911!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfac6a42994c21fdd!2sPT%20Adita%20Nusantara%20Solution!5e0!3m2!1sen!2sid!4v1673149099363!5m2!1sen!2sid"
+                                frameborder="0" style="height: 230px; border:0;" allowfullscreen="" aria-hidden="false"
+                                tabindex="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light mt-5 py-5">
+        <div class="container pt-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h3 class="text-white mb-4">Contact Us</h3>
+                    <p class="mb-2"><i class="bi bi-geo-alt text-primary me-2"></i>Jl. Maulana Hasanudin No.38A, Kp. Cipondoh Rt. 005 Rw. 008 Cipondoh Kota Tangerang 15148 Email: info@adita-ns.com Phone: 021 55712013</p>
+                    <p class="mb-2"><i class="bi bi-envelope-open text-primary me-2"></i>info@adita-ns.com</p>
+                    <p class="mb-0"><i class="bi bi-telephone text-primary me-2"></i>021 55712013</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                <h3 class="text-white mb-4">Follow Us</h3>
+                    <div class="d-flex">
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-facebook-f fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded me-2" href="#"><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded" href="#"><i class="fab fa-instagram fw-normal"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+
+</body>
+</html>
